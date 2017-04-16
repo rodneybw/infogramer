63bbe4393ca6db03a69886c9fc58327e80d64888
rodneybw <meuter.arno@gmail.com>
2017-04-17 00:07:03 +0200

    command line arguments 'message' and 'importance'

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
 
+	message := flag.String("message", "", "Message")
+	importance := flag.String("importance", "low", "Importance: (low|normal|high)")
+	flag.Parse()
+
+	fmt.Printf("Message: %s\nImportance: %s\n", *message, *importance)
 }
